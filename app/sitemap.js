550a80e1c0c5f66ffe4aaed8eb317c562a2d5c4c
Hemadhri-L <hemadhril72@gmail.com>
2026-03-01 18:34:47 +0530

    fix sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,7 +1,7 @@
 export default function sitemap() {
   return [
     {
-      url: "https://slidewizard-nine.vercel.app/",
+      url: "https://slidewizard-nine.vercel.app",
       lastModified: new Date(),
     },
   ];
